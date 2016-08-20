9606dfd6cde9f22b47987ba5a192b40498555657
Steph Wang <sw.steph@gmail.com>
2016-08-20 18:25:24 +0000

    initial setup and  landing page

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+// home page
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// redirect for valid short urls
+
+// create new short url
 
 app.listen(process.env.PORT || 8080);
